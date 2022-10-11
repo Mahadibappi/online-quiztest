30aae45c5148255ff847bca0c8c0f95e8a848392
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-10-11 14:34:36 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./Components/About/About";
+import Blog from "./Components/Blog/Blog";
+import Home from "./Components/Home/Home";
+import Statistics from "./Components/Statistics/Statistics";
+import Topics from "./Components/Topics/Topics";
+import Main from "./Layout/Main";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      children: [
+        { path: "/home", element: <Home /> },
+        { path: "/topics", element: <Topics /> },
+        { path: "/statistics", element: <Statistics /> },
+        { path: "/blog", element: <Blog /> },
+        { path: "/about", element: <About /> },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <h3> react quiz app to be started</h3>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Components/NavBar/Nav.js
@@ -0,0 +1,212 @@
+import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import img from "../../icon/logo.png";
+const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="bg-green-700">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <NavLink
+            to="/"
+            aria-label="Company"
+            title="Company"
+            className="inline-flex items-center"
+          >
+            <img src={img} className="w-10" alt="image" />
+            <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+              Coders Choice
+            </span>
+          </NavLink>
+          <ul className="flex items-center hidden space-x-8 lg:flex">
+            <li>
+              <NavLink
+                to="/home"
+                aria-label="Our product"
+                title="Our product"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/topics"
+                aria-label="Our product"
+                title="Our product"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                Topics
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/statistics"
+                aria-label="Product pricing"
+                title="Product pricing"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                Statistics
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/blog"
+                aria-label="About us"
+                title="About us"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                aria-label="About us"
+                title="About us"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                About us
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                aria-label="Sign up"
+                title="Sign up"
+              >
+                Sign up
+              </NavLink>
+            </li>
+          </ul>
+          <div className="lg:hidden">
+            <button
+              aria-label="Open Menu"
+              title="Open Menu"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                />
+              </svg>
+            </button>
+            {isMenuOpen && (
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-white border rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
+                    <div>
+                      <a
+                        to="/"
+                        aria-label="Company"
+                        title="Company"
+                        className="inline-flex items-center"
+                      >
+                        <svg
+                          className="w-8 text-deep-purple-accent-400"
+                          viewBox="0 0 24 24"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeMiterlimit="10"
+                          stroke="currentColor"
+                          fill="none"
+                        >
+                          <rect x="3" y="1" width="7" height="12" />
+                          <rect x="3" y="17" width="7" height="6" />
+                          <rect x="14" y="1" width="7" height="6" />
+                          <rect x="14" y="11" width="7" height="12" />
+                        </svg>
+                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                          Coders Choice
+                        </span>
+                      </a>
+                    </div>
+                    <div>
+                      <button
+                        aria-label="Close Menu"
+                        title="Close Menu"
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <img src={img} className="w-10" alt="image" />
+                      </button>
+                    </div>
+                  </div>
+                  <nav>
+                    <ul className="space-y-4">
+                      <li>
+                        <NavLink
+                          to="/home"
+                          aria-label="Our product"
+                          title="Our product"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >
+                          Home
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/topics"
+                          aria-label="Our product"
+                          title="Our product"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >
+                          Topics
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/statistics"
+                          aria-label="Product pricing"
+                          title="Product pricing"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >
+                          Statistics
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/about"
+                          aria-label="About us"
+                          title="About us"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >
+                          About us
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/"
+                          className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                          aria-label="Sign up"
+                          title="Sign up"
+                        >
+                          Sign up
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
